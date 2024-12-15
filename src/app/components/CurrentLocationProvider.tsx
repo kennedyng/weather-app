102ae44a8ca9792d81c34b7096067d1264b678f6
kennedy ngosa <kennedyngosachanda@gmail.com>
2024-12-15 13:00:52 +0200

    chore: remove unsed vars

--- src/app/components/CurrentLocationProvider.tsx
+++ src/app/components/CurrentLocationProvider.tsx
@@ -12,12 +12,6 @@ const CurrentLocationProvider: React.FC<Props> = ({ children }) => {
   const dispatch = useAppDispatch();
   const { location, unit } = useAppSelector((state) => state.userInputsReducer);
 
-  const { isLoading, isFetching, data, error } = useGetCurrentWeatherQuery({
-    lat: String(location.lat),
-    lon: String(location.lon),
-    unity: unit,
-  });
-
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) =>
